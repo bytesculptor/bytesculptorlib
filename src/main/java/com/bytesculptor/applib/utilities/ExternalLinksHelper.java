c5a3fa602dde211a27ff4303016504a2977efa6f
Byte_Sculptor <bytesculptor@gmail.com>
2021-02-07 18:40:52 +0000

    - update to Kotlin 1.4.30 - copyright update

--- src/main/java/com/bytesculptor/applib/utilities/ExternalLinksHelper.java
+++ src/main/java/com/bytesculptor/applib/utilities/ExternalLinksHelper.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2017 - 2021  Byte Sculptor Software  - All Rights Reserved
+ *
+ * All information contained herein is and remains the property of Byte Sculptor Software.
+ * Unauthorized copying of this file, via any medium, is strictly prohibited unless prior
+ * written permission is obtained from Byte Sculptor Software.
+ *
+ * Romeo Rondinelli - bytesculptor@gmail.com
+ *
+ */
+
 package com.bytesculptor.applib.utilities;
 
 import android.content.ActivityNotFoundException;
